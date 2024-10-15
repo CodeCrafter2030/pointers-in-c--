78103de3264cc2c5085dcd94155530a2ea15b8d4
CodeCrafter2030 <166577132+CodeCrafter2030@users.noreply.github.com>
2024-10-15 21:39:38 +0530

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -8,7 +8,7 @@ int main()
 int a=3;
 int* b=&a; //here pointer b is created which stores the address of a
 cout<<*b<<endl;  // '*' is Dereferencing operator 
-           // '&' is addres of operator 
+           // '&' is address of operator 
 
 // //Pointer in pointer
 int** c=&b; //here pointer c is created which stores the address of pointer b
